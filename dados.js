c521554780a868dbb3cc9da4de3da0e78d3a1a35
cisotar <contato.tarciso@gmail.com>
2026-01-04 17:39:40 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+// Arquivo: dados.js
+var itensIniciais = [
+  "Abacate", "Abacaxi", "Acerola", "Ameixa Preta", "Ameixa Vermelha", 
+  "Amora", "Banana da Terra", "Banana Maçã", "Banana Nanica", 
+  "Banana Prata", "Caju", "Caqui Chocolate", "Caqui Fuyu", 
+  "Caqui Rama Forte", "Carambola", "Cereja", "Coco Seco", "Coco Verde", 
+  "Figo Roxo", "Framboesa", "Goiaba Branca", "Goiaba Vermelha", 
+  "Graviola", "Jabuticaba", "Kiwi", "Laranja Bahia", "Laranja Lima", 
+  "Laranja Pera", "Lichia", "Limão Galego", "Limão Siciliano", 
+  "Limão Taiti", "Maçã Fuji", "Maçã Gala", "Maçã Red", "Maçã Verde", 
+  "Mamão Formosa", "Mamão Papaya", "Manga Espada", "Manga Palmer", 
+  "Manga Rosa", "Manga Tommy", "Maracujá Azedo", "Maracujá Doce", 
+  "Melancia", "Melão Amarelo", "Melão Cantaloupe", "Melão Pele de Sapo", 
+  "Mexerica", "Morango", "Murcott", "Nectarina", "Pera D'Água", 
+  "Pera Portuguesa", "Pera Williams", "Pêssego", "Pinha (Fruta-do-conde)", 
+  "Pitaya", "Romã", "Tangerina Ponkan", "Uva Itália", "Uva Niágara", 
+  "Uva Rubi", "Uva Thompson"
+];
